97e846ae8b4696e3878528475c8adba054f0b4e1
kentakom1213 <kentakom1213@gmail.com>
2026-02-18 22:01:17 +0900

    add: 精進

--- src/bin/abc413-d.rs
+++ src/bin/abc413-d.rs
@@ -1,14 +1,7 @@
 #![allow(non_snake_case)]
 
-use std::collections::HashMap;
-
-use cp_library_rs::{
-    chmin, debug,
-    number_theory::frac::Frac,
-    utils::{boolutil::BoolUtil, consts::Infinity},
-};
+use cp_library_rs::{debug, utils::boolutil::BoolUtil};
 use itertools::Itertools;
-use num::Zero;
 use proconio::input;
 
 fn main() {
@@ -16,73 +9,36 @@ fn main() {
         T: usize,
     }
     for _ in 0..T {
-        solve();
+        println!("{}", solve().yesno());
     }
 }
 
-fn solve() {
+fn solve() -> bool {
     input! {
         N: usize,
         mut A: [i64; N]
     }
+
     if N == 2 {
-        println!("Yes");
-        return;
+        return true;
     }
 
-    A.sort_unstable();
+    // 絶対値が全て同じ場合
+    if A.iter().all(|v| v.abs() == A[0].abs()) {
+        // 個数をカウント
+        let plus = A.iter().filter(|v| **v > 0).count();
+
+        return plus == 0 || plus == N || plus.abs_diff(N - plus) <= 1;
+    }
+
+    // 絶対値でソート
+    A.sort_unstable_by_key(|v| v.abs());
 
     debug!(A);
 
-    // 一番小さい公比を見つける
-    let r = {
-        let mut x = (i64::infinity(), true);
-        let mut r = Frac::new(i64::infinity(), 1);
-
-        for (&a, &b) in A.iter().tuple_windows() {
-            let f = Frac::new(a, b);
-            if chmin!(x, (f.numer().abs(), f >= Frac::zero())) {
-                r = f;
-            }
-            if chmin!(x, (f.denom().abs(), f >= Frac::zero())) {
-                r = f;
-            }
-        }
-        r
-    };
-
-    debug!(r);
-
-    // 逆インデックス
-    let mut R = HashMap::<i64, Vec<usize>>::new();
-    // 0 は追加しない
-    for (&a, i) in A[1..].iter().zip(1..) {
-        R.entry(a).or_default().push(i);
-    }
-
-    // 到達可能性をチェック
-    let mut isok = vec![false; N];
-    let mut q = vec![0];
-    while let Some(i) = q.pop() {
-        isok[i] = true;
-        if let Some(suc) = (r * A[i]).as_integer() {
-            if let Some(idxs) = R.get_mut(&suc) {
-                if let Some(i) = idxs.pop() {
-                    q.push(i);
-                }
-            }
-        }
-        if let Some(pre) = (Frac::new(A[i], 1) / r).as_integer() {
-            if let Some(idxs) = R.get_mut(&pre) {
-                if let Some(i) = idxs.pop() {
-                    q.push(i);
-                }
-            }
-        }
-        debug!(q);
-    }
-
-    debug!(isok);
-
-    println!("{}", isok.into_iter().all(|x| x).yesno());
+    A[1..]
+        .iter()
+        .tuple_windows()
+        // a/b == A[0]/A[1]
+        .all(|(&a, &b)| A[1] * a == A[0] * b)
 }

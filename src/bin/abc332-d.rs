7214afc8194a811a08df19b243498aaf6d3fc7f0
kentakom1213 <kentakom1213@gmail.com>
2026-02-18 14:26:48 +0900

    add: 精進

--- src/bin/abc332-d.rs
+++ src/bin/abc332-d.rs
@@ -1,6 +1,9 @@
 #![allow(non_snake_case)]
 
+use std::collections::VecDeque;
+
 use proconio::input;
+use rustc_hash::FxHashSet;
 
 fn main() {
     input! {
@@ -10,5 +13,46 @@ fn main() {
         B: [[u32; W]; H],
     }
 
-    
+    let mut visited = FxHashSet::default();
+    let mut q = VecDeque::new();
+    q.push_back((A, 0));
+
+    while let Some((arr, d)) = q.pop_front() {
+        if &arr == &B {
+            println!("{d}");
+            return;
+        }
+        for r in 0..H - 1 {
+            let new = swap_row(&arr, r);
+            if visited.contains(&new) {
+                continue;
+            }
+            visited.insert(new.clone());
+            q.push_back((new, d + 1));
+        }
+        for c in 0..W - 1 {
+            let new = swap_col(&arr, c);
+            if visited.contains(&new) {
+                continue;
+            }
+            visited.insert(new.clone());
+            q.push_back((new, d + 1));
+        }
+    }
+
+    println!("-1");
+}
+
+fn swap_row(arr: &Vec<Vec<u32>>, r: usize) -> Vec<Vec<u32>> {
+    let mut res = arr.clone();
+    res.swap(r, r + 1);
+    res
+}
+
+fn swap_col(arr: &Vec<Vec<u32>>, c: usize) -> Vec<Vec<u32>> {
+    let mut res = arr.clone();
+    for row in res.iter_mut() {
+        row.swap(c, c + 1);
+    }
+    res
 }
